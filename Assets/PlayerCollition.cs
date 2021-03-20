ee8e3a66aff70f8d7f6d104773eddaff50cf280d
BAZIG <45736195+BAZIG@users.noreply.github.com>
2021-03-20 21:30:19 +0100

    1Proj
    Projet de base attention encore des bugs en code pour jump

---
+++ Assets/PlayerCollition.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerCollition : MonoBehaviour
+{
+    private Vector3 velocity;
+    void OnCollisionEnter(Collision collisionInfo){
+        if(collisionInfo.collider.name == "Obstacle")
+        {
+            transform.position = new Vector3(0, 1.01f, -41.07f);
+            velocity.y = 0f;
+            velocity.x = 0f;
+            velocity.z = 0f;
+        }
+    }
+    void Update(){
+        if (transform.position.y < -1) // where -10 is the lowest the player can fall before reset
+        {
+        transform.position = new Vector3(0, 0.37f, -7.46f); // same as new Vector3(0, 0, 0);
+        }
+    }
+}
---
+++ Assets/PlayerMovements.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 1000f;
+    public float upwaysForce = 1000f;
+    public float jumpHeight = 7f;
+    public bool isGrounded;
+    public float NumberJumps = 0f;
+    public float MaxJumps = 2;
+
+    void Update()
+    {
+        if (NumberJumps > MaxJumps - 1)
+        {
+            isGrounded = false;
+        }
+
+        if (isGrounded)
+        {
+            if (Input.GetButtonDown(KeyCode.Space))
+            {
+                rb.AddForce(0, sidewaysForce* Time.deltaTime, 0);
+                NumberJumps += 1;
+            }
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(sidewaysForce* Time.deltaTime, 0 , 0);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(0, 0 , -sidewaysForce* Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(0, 0 , sidewaysForce* Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-sidewaysForce* Time.deltaTime, 0 , 0);
+        }
+        if(0.35 < transform.position.y && transform.position.y < 0.37 && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(0,upwaysForce* Time.deltaTime, 0);
+            Debug.Log("Jump");
+            
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        isGrounded = true;
+        NumberJumps = 0;
+    }
+    void OnCollisionExit(Collision other)
+        {
+            
+    }
+}
