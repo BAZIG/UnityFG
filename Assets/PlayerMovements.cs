a7744e4ae701d1a4d7658119cfd3ced9b2d8a9f0
BAZIG <45736195+BAZIG@users.noreply.github.com>
2021-03-20 23:14:29 +0100

    Jump modification
    Jump with time delay

--- Assets/PlayerMovements.cs
+++ Assets/PlayerMovements.cs
@@ -2,32 +2,13 @@
 
 public class PlayerMovements : MonoBehaviour
 {
+    public float canJump = 0f;
     public Rigidbody rb;
     public float forwardForce = 2000f;
     public float sidewaysForce = 1000f;
     public float upwaysForce = 1000f;
-    public float jumpHeight = 7f;
-    public bool isGrounded;
-    public float NumberJumps = 0f;
-    public float MaxJumps = 2;
 
-    void Update()
-    {
-        if (NumberJumps > MaxJumps - 1)
-        {
-            isGrounded = false;
-        }
 
-        if (isGrounded)
-        {
-            if (Input.GetButtonDown(KeyCode.Space))
-            {
-                rb.AddForce(0, sidewaysForce* Time.deltaTime, 0);
-                NumberJumps += 1;
-            }
-        }
-    }
-    // Update is called once per frame
     void FixedUpdate()
     {
 
@@ -47,20 +28,11 @@ public class PlayerMovements : MonoBehaviour
         {
             rb.AddForce(-sidewaysForce* Time.deltaTime, 0 , 0);
         }
-        if(0.35 < transform.position.y && transform.position.y < 0.37 && Input.GetKeyDown(KeyCode.Space))
+        if(Time.time > canJump && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(0,upwaysForce* Time.deltaTime, 0);
             Debug.Log("Jump");
-            
+            canJump = Time.time + 0.3f;    
         }
-    }
-    void OnCollisionEnter(Collision other)
-    {
-        isGrounded = true;
-        NumberJumps = 0;
-    }
-    void OnCollisionExit(Collision other)
-        {
-            
     }
 }
